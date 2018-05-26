3cd41724d0883373a98bcbec0955ea760f910b13
SinoaggZhangXiangLan <Zhangxl@sinoagg.com>
2018-05-26 13:41:46 +0800

    OTSU object detection

---
+++ PassengerFlow5/CMakeFiles/pa_flow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa_flow.dir/src/test/main.cpp.o"
+  "pa_flow.pdb"
+  "pa_flow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pa_flow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
